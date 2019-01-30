e8af0f9df5245631c70b1312edf30d91fd08f8f8
elyzinger <45950112+elyzinger@users.noreply.github.com>
2019-01-30 21:20:15 +0200

    Add files via upload

---
+++ Person.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ely_1
+{
+    class Person
+    {
+        public string _id;
+        public string _name;
+        public int _birth;
+        public float _height;
+
+        public Person(string id, string name, int birth, float height)
+        {
+            _id = id;
+            _name = name;
+            _birth = birth;
+            _height = height;
+        }
+        public Person(string id)
+        {
+            _id = id;
+        }
+        public Person()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            string result = $"name: {_name} id: {_id} birth: {_birth} height: {_height} ";
+            return result;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ely_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person("3333", "bebe", 1986, 1.8f);
+
+            Console.WriteLine(p);
+        }
+        
+    }
+}
